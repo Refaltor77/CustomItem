da4929d7679ac3d81cfc20186fdc1db502d358ce
Refaltor <77900181+Refaltor77@users.noreply.github.com>
2021-06-24 23:33:33 +0200

    Update Register.php

--- src/refaltor_Natof/CustomItem/Register.php
+++ src/refaltor_Natof/CustomItem/Register.php
@@ -2,8 +2,11 @@
 
 namespace refaltor_Natof\CustomItem;
 
+use core\API\Items;
 use pocketmine\event\Listener;
 use pocketmine\event\server\DataPacketReceiveEvent;
+use pocketmine\event\server\DataPacketSendEvent;
+use pocketmine\network\mcpe\protocol\ContainerClosePacket;
 use pocketmine\network\mcpe\protocol\StartGamePacket;
 use pocketmine\plugin\PluginBase;
 use refaltor_Natof\Customitem\Loader\Loader;
@@ -16,18 +19,49 @@ class Register extends PluginBase implements Listener
     public function onEnable(){
         self::$instance = $this;
         $this->saveResource('config.yml');
+		Loader::register();
         $this->getServer()->getPluginManager()->registerEvents($this, $this);
-        Loader::register();
     }
 
     public static function getInstance(): self{
         return self::$instance;
     }
 
-    public function onDataReceive(DataPacketReceiveEvent $event){
+	/** @var bool */
+	private $cancel_send = true;
+
+
+
+	public function onPacket(DataPacketSendEvent $event){
 		$packet = $event->getPacket();
 		if ($packet instanceof StartGamePacket) {
 			$packet->itemTable = Loader::$entries;
+		}
+	}
+
+
+	/**
+	 * @param DataPacketSendEvent $event
+	 * @priority NORMAL
+	 * @ignoreCancelled true
+	 */
+	public function onDataPacketSend(DataPacketSendEvent $event) : void{
+		if($this->cancel_send && $event->getPacket() instanceof ContainerClosePacket){
+			$event->setCancelled();
+		}
+	}
+
+
+	/**
+	 * @param DataPacketReceiveEvent $event
+	 * @priority NORMAL
+	 * @ignoreCancelled true
+	 */
+	public function onDataPacketReceive(DataPacketReceiveEvent $event) : void{
+		if($event->getPacket() instanceof ContainerClosePacket){
+			$this->cancel_send = false;
+			$event->getPlayer()->sendDataPacket($event->getPacket(), false, true);
+			$this->cancel_send = true;
 		}
 	}
 }
